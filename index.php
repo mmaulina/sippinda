c08f267661032e0ba22af51f940d18292cb2aa92
Muhammad Irwan Firdaus <104238625+MuhammadIrwanFirdaus@users.noreply.github.com>
2025-06-11 11:42:08 +0800

    awal

---
+++ index.php
@@ -0,0 +1,230 @@
+<?php
+session_start();
+if (!isset($_SESSION['id_user'])) {
+    header('location:login/login.php');
+}
+include "koneksi.php";
+include "template/header.php";
+include 'template/alert.php'; 
+?>
+<style>
+    .row {
+        display: flex;
+        /* Menggunakan Flexbox untuk baris */
+        flex-wrap: nowrap;
+        /* Mencegah baris membungkus */
+    }
+
+    .sidebar {
+        width: 250px;
+        /* Lebar sidebar */
+        height: 100vh;
+        /* Tinggi sidebar sesuai dengan tinggi viewport */
+        position: sticky;
+        /* Membuat sidebar tetap di posisi saat scroll */
+        top: 0;
+        /* Menjaga sidebar tetap di atas saat scroll */
+        overflow-y: auto;
+        /* Menambahkan scroll jika konten lebih tinggi dari viewport */
+    }
+
+    .main-content {
+        flex: 1;
+        /* Membuat konten utama mengambil sisa ruang */
+        padding: 20px;
+        /* Menambahkan padding untuk konten */
+        background-color: #f8f9fa;
+        /* Warna latar belakang konten */
+        overflow-y: auto;
+        /* Menambahkan scroll jika konten lebih tinggi dari viewport */
+    }
+
+    @media (max-width: 768px) {
+        .sidebar {
+            width: 100%;
+            /* Sidebar penuh lebar pada layar kecil */
+            height: auto;
+            /* Tinggi otomatis pada layar kecil */
+            position: relative;
+            /* Mengubah posisi pada layar kecil */
+        }
+    }
+</style>
+<link rel="stylesheet" href="assets/fa/css/all.min.css">
+<link rel="stylesheet" href="assets/css/bootstraps.min.css">
+<script src="assets/js/bootstrap.min.js"></script>
+<div class="container-fluid">
+    <div class="row"> <!-- Mulai baris untuk sidebar dan konten -->
+
+        <!-- Sidebar -->
+        <?php include "template/sidebar.php"; ?>
+
+        <!-- Konten utama -->
+        <div class="col-md-9 col-lg-10 main-content"> <!-- Lebar konten agar sejajar dengan sidebar -->
+            <?php
+            $page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
+            switch ($page) {
+                // perusahaan
+                case "profil_perusahaan":
+                    include "perusahaan/tampil.php";
+                    break;
+                case "profil_admin":
+                    include "perusahaan/tampil_admin.php";
+                    break;
+                case "tambah_profil":
+                    include "perusahaan/tambah_profil.php";
+                    break;
+                case "update_profil":
+                    include "perusahaan/update_profil.php";
+                    break;
+                case "update_profil_admin":
+                    include "perusahaan/update_profil_admin.php";
+                    break;
+                case "delete_profil":
+                    include "perusahaan/delete_profil.php";
+                    break;
+                case "delete_profil_admin":
+                    include "perusahaan/delete_profil_admin.php";
+                    break;
+                case "excel_profil":
+                    include "perusahaan/ekspor.php";
+                    break;
+
+                // data pembangkit dan data teknis pembangkit
+                case "pembangkit":
+                    include "pembangkit/tampil.php";
+                    break;
+                case "pembangkit_tambah":
+                    include "pembangkit/tambah.php";
+                    break;
+                case "pembangkit_edit":
+                    include "pembangkit/update.php";
+                    break;
+                case "pembangkit_hapus":
+                    include "pembangkit/delete.php";
+                    break;
+                case "pembangkit_export":
+                    include "pembangkit/export.php";
+                    break;
+
+                // laporan perbulan
+                case "laporan_perbulan":
+                    include "laporan_perbulan/tampil.php";
+                    break;
+                case "tambah_laporan_perbulan":
+                    include "laporan_perbulan/tambah_laporan.php";
+                    break;
+                case "tambah_laporan_perbulan2":
+                    include "laporan_perbulan/tambah_laporan2.php";
+                    break;
+                case "tambah_unit":
+                    include "laporan_perbulan/tambah_unit.php";
+                    break;
+                case "edit_laporan_perbulan":
+                    include "laporan_perbulan/update.php";
+                    break;
+                case "hapus_laporan_perbulan":
+                    include "laporan_perbulan/hapus.php";
+                    break;
+                case "excel_laporan_bulanan":
+                    include "laporan_perbulan/ekspor.php";
+                    break;
+                    
+
+                // laporan persemester
+                case "laporan_persemester":
+                    include "laporan_persemester/tampil.php";
+                    break;
+                case "tambah_parameter":
+                    include "laporan_persemester/tambah_parameter.php";
+                    break;
+                    case "edit_parameter":
+                    include "laporan_persemester/edit_parameter.php";
+                    break;
+                    case "hapus_parameter":
+                    include "laporan_persemester/hapus_parameter.php";
+                    break;
+                case "tambah_laporan_persemester":
+                    include "laporan_persemester/tambah_laporan.php";
+                    break;
+                case "edit_laporan_persemester":
+                    include "laporan_persemester/edit_laporan.php";
+                    break;
+                case "hapus_laporan_persemester":
+                    include "laporan_persemester/hapus_laporan.php";
+                    break;
+
+                // pengguna
+                case "pengguna":
+                    include "pengguna/tampil.php";
+                    break;
+                case "pengguna_tambah_admin":
+                    include "pengguna/tambah_admin.php";
+                    break;
+                case "pengguna_edit_admin":
+                    include "pengguna/edit_admin.php";
+                    break;
+                case "edit_password":
+                    include "pengguna/edit_password.php";
+                    break;
+                case "pengguna_hapus_admin":
+                    include "pengguna/hapus_admin.php";
+                    break;
+
+                //news
+                case "upload":
+                    include "news/tambah.php";
+                    break;
+                case "tabel":
+                    include "news/tabel.php";
+                    break;
+                case "edit_konten":
+                    include "news/edit.php";
+                    break;
+                case "hapus_konten":
+                    include "news/hapus.php";
+                    break;
+
+                //djih
+                case "upload_djih":
+                    include "djih/tambah.php";
+                    break;
+                case "tabel_djih":
+                    include "djih/tabel.php";
+                    break;
+                case "edit_konten_djih":
+                    include "djih/edit.php";
+                    break;
+                case "hapus_konten_djih":
+                    include "djih/hapus.php";
+                    break;
+                case "tampil_konten_djih":
+                    include "djih/tampil.php";
+                    break;
+
+                //notiifikasi
+                case "notifikasi":
+                    include "notif.php";
+                    break;
+
+                //kontak
+                case "kontak":
+                    include "kontak/tampil.php";
+                    break;
+                case "update_kontak":
+                    include "kontak/edit_kontak.php";
+                    break;
+
+
+                // default saat login berhasil
+                default:
+                    include "dashboard.php";
+                    break;
+            }
+            ?>
+        </div>
+
+    </div> <!-- Tutup row -->
+</div> <!-- Tutup container-fluid -->
+
+<?php include "template/footer.php"; ?>
---
+++ login/login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="icon" href="../assets/img/kalsel.png" type="image/png">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+
+<body class="d-flex justify-content-center align-items-center vh-100">
+    <div class="card shadow p-4" style="width: 400px;">
+        <div class="d-flex justify-content-between mb-3">
+            <img src="../assets/img/kalsel.png" alt="Logo Kalsel" style="width: 50px;">
+            <img src="../assets/img/LOGO KEMENTERIAN PERINDUSTRIAN.png" alt="Logo Kementerian" style="width: 80px;">
+        </div>
+        <h3 class="text-center"><i class="me-1">Login SIPPINDA</h3>
+        <hr>
+        <?php if (!empty($error)) : ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo $error; ?>
+            </div>
+        <?php endif; ?>
+
+        <form method="POST" action="">
+            <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>">
+
+            <div class="mb-3">
+                <label for="username" class="form-label">Username atau Email</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Masukkan username atau email" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <div class="input-group">
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Masukkan password" required>
+                    <span class="input-group-text" onclick="togglePassword()" style="cursor:pointer;">
+                        <i class="bi bi-eye-slash" id="toggleIcon"></i>
+                    </span>
+                </div>
+            </div>
+            <button type="submit" class="btn custom-btn w-100">Login</button>
+        </form>
+
+        <div class="text-center mt-3">
+            <a href="daftar.php">Belum punya akun?</a>
+        </div>
+    </div>
+
+    <script>
+        function togglePassword() {
+            const passwordField = document.getElementById("password");
+            const toggleIcon = document.getElementById("toggleIcon");
+
+            const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+            passwordField.setAttribute("type", type);
+
+            toggleIcon.classList.toggle("bi-eye");
+            toggleIcon.classList.toggle("bi-eye-slash");
+        }
+    </script>
+
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+
+    </script>
+
+
+</body>
+
+</html>
