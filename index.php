8a5d4f3cbbbd94875266bb749da416aac75b19f1
Muhammad Irwan Firdaus <104238625+MuhammadIrwanFirdaus@users.noreply.github.com>
2025-06-13 16:58:01 +0800

    c

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-// if (!isset($_SESSION['id_user'])) {
+if (!isset($_SESSION['id_user'])) {
     // header('location:home.php');
-//     header('location:login/login.php');
-// }
+    header('location:login/login.php');
+}
 include "koneksi.php";
 include "template/header.php";
 // include 'template/alert.php'; 
