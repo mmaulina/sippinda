b226ed1386abb267cf9ca7c807988506c5c33552
Maya Maulina <mayamaulina16@gmail.com>
2025-06-14 22:19:18 +0800

    pewarnaan

--- template/header.php
+++ template/header.php
@@ -25,7 +25,7 @@
 
 <body id="page-top">
     <!-- Topbar -->
-    <nav class="navbar navbar-expand navbar-light bg-gradient-danger topbar mb-4 static-top shadow">
+    <nav class="navbar navbar-expand navbar-dark bg-gradient-danger topbar mb-4 static-top shadow">
 
         <!-- Sidebar Toggle (Topbar) -->
         <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
@@ -182,9 +182,10 @@
             <li class="nav-item dropdown no-arrow">
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                    <img class="img-profile rounded-circle"
-                        src="img/undraw_profile.svg">
+                    <span class="mr-2 d-none d-lg-inline text-gray-200 small">Douglas McGee</span>
+                    <i class="fas fa-user fa-sm"></i>
+                    <!-- <img class="img-profile rounded-circle"
+                        src="img/undraw_profile.svg"> -->
                 </a>
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
