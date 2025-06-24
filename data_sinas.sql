c7c6a6a8ca263a84106e02e59fcc5029dae1b8d4
Muhammad Irwan Firdaus <104238625+MuhammadIrwanFirdaus@users.noreply.github.com>
2025-06-24 21:19:05 +0800

    sql

--- data_sinas.sql
+++ data_sinas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 21, 2025 at 12:01 PM
+-- Generation Time: Jun 24, 2025 at 01:18 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -34,9 +34,17 @@ CREATE TABLE `data_sinas` (
   `upload` varchar(225) NOT NULL,
   `triwulan` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `tahun` varchar(225) NOT NULL,
+  `jenis_pelaporan` enum('Tahap Produksi','Tahap Konstruksi') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `status` enum('diterima','diajukan','dikembalikan') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `keterangan` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `data_sinas`
+--
+
+INSERT INTO `data_sinas` (`id`, `id_user`, `nama_perusahaan`, `upload`, `triwulan`, `tahun`, `jenis_pelaporan`, `status`, `keterangan`) VALUES
+(5, 1, 'WASAKA CODE DIGITAL DEVELOPMENT', 'uploads/WASAKACODEDIGITALDEVELOPMENT_20250624_102249_9cdd1e.png', 'Triwulan II 2025', '2025', 'Tahap Produksi', 'diterima', '-');
 
 --
 -- Indexes for dumped tables
@@ -56,7 +64,7 @@ ALTER TABLE `data_sinas`
 -- AUTO_INCREMENT for table `data_sinas`
 --
 ALTER TABLE `data_sinas`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
